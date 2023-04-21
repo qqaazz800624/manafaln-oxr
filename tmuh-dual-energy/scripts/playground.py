57f6a92598341941a7c6728c17db613b73a991b1
qqaazz800624 <qqaazz800624@gmail.com>
2023-04-21 18:29:23 +0800

    practice in preprocessing

--- tmuh-dual-energy/scripts/playground.py
+++ tmuh-dual-energy/scripts/playground.py
@@ -8,12 +8,14 @@ from sklearn.model_selection import KFold
 from tqdm import tqdm
 #%%
 
-# Count how many types of labels
-# class = 6
-
 root = "/neodata/oxr/tmuh/label"
 uids = os.listdir(root)
 labels = glob(f'{root}/*.json')
+
+#%%
+
+# Count how many types of labels
+# class = 6
 
 strokeColor = []
 for i in range(len(labels)):
@@ -25,6 +27,27 @@ for i in range(len(labels)):
 
 unique_class = list(set(strokeColor))
 print(unique_class)
+
+#%%
+# create a dictionary for each strokeColor and its corresponding annotation tag
+
+tagpair = []
+for i in range(len(labels)):
+    with open(labels[i]) as f:
+        data = json.load(f)
+    shapes = data['shapes']
+    for j in range(len(shapes)):
+        tagpair.append([shapes[j].get('strokeColor'), shapes[j].get('tagAnno')])
+
+tag_dict = {}
+for tag in unique_class:
+    taganno = []
+    for i in range(len(tagpair)):
+        if tagpair[i][0] == tag:
+            taganno.append(tagpair[i][1])
+    tag_dict[tag] = list(set(taganno))
+
+tag_dict
 
 #%%
 
